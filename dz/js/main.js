fdd01c2f10e2696aeb00e423ffd116d7ea33416d
NKV-88 <sysadm86@gmail.com>
2019-03-15 11:24:22 +0500

    Затуп с <template>

--- dz/js/main.js
+++ dz/js/main.js
@@ -1,7 +1,15 @@
 // 1.Есть кнопка button рядом с ней есть текст (Hello) необходимо при нажатии на кнопку поменять текст кнопки на (Привет)
 
 var button = document.querySelector("#button");
+var text = document.querySelector("#textTrue").content;
 button.onclick = function () {
-	document.querySelector("#text").innerText = text_true[1];
+	document.querySelector("#button").innerText = text;
 };
-console.log(button);
+
+//2.Есть кнопка button рядом с ней есть тэг <b></b>(Hello) необходимо при нажатии на кнопку поменять тэг на <h3></h3>
+
+var button2 = document.querySelector("#button2");
+var tag = document.querySelector("#trueTag").content;
+button2.onclick = function () {
+	document.querySelector("#changedTag").innerText = tag;
+};
