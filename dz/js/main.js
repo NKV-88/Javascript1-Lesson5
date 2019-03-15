afe5e3e6dc99b9063faa634b105c0b2f729b7e66
NKV-88 <sysadm86@gmail.com>
2019-03-15 16:14:38 +0500

    Закончил 2 задания без template

--- dz/js/main.js
+++ dz/js/main.js
@@ -1,15 +1,15 @@
 // 1.Есть кнопка button рядом с ней есть текст (Hello) необходимо при нажатии на кнопку поменять текст кнопки на (Привет)
 
 var button = document.querySelector("#button");
-var text = document.querySelector("#textTrue").content;
+//var text = document.querySelector("#temp").content.cloneNode(true);
 button.onclick = function () {
-	document.querySelector("#button").innerText = text;
+	button.innerText = 'Привет';
 };
 
 //2.Есть кнопка button рядом с ней есть тэг <b></b>(Hello) необходимо при нажатии на кнопку поменять тэг на <h3></h3>
 
 var button2 = document.querySelector("#button2");
-var tag = document.querySelector("#trueTag").content;
+//var tag = document.querySelector("#trueTag").content.cloneNode(true);
 button2.onclick = function () {
-	document.querySelector("#changedTag").innerText = tag;
+	document.querySelector("#changedTag").innerHTML = "<h3>h3h3</h3>";
 };
